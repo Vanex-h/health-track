45dcd3005cb1e64d09ed22a6690a5a48a09a542d
Vanex-h <vanessahirwa5@gmail.com>
2023-11-09 16:28:03 +0200

    With RFID

--- HealthTrack-backend/python/data_handler.py
+++ HealthTrack-backend/python/data_handler.py
@@ -1,29 +1,32 @@
 import serial
 import requests
 
-ser = serial.Serial("COM6", 9600, timeout=1)
+ser = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
 
-api_url = "http://localhost:1400/record"
+# API endpoint URL
+api_url = "http://localhost:1400/health-track/record"
 
 try:
     while True:
         data = ser.readline().decode().strip()
         if data:
-            # Split the data string into components
-            components = data.split(" && ")
-            # Extract heart rate and body temperature
-            heart_rate = float(components[0].split(": ")[1])
-            body_temperature = float(components[1].split(": ")[1])
+            print(data);
+            if "Patient ID:" in data and "Heart Rate:" in data and "Body Temperature:" in data:
+                # Split the data string into components
+                components = data.split(" && ")
+                # Extract patient ID, heart rate and body temperature
+                patient_id = components[0].split(": ")[1]
+                heart_rate = float(components[1].split(": ")[1])
+                body_temperature = float(components[2].split(": ")[1])
 
-
-            if 60 <= heart_rate <= 120:
-                print(f"Received data from Arduino: {data}")
-                request_body = {"patient_id": "1", "heart_rate": int(float(heart_rate)), "body_temprature": int(float(body_temperature))}
-                response = requests.post(api_url, json=request_body)
-                if response.status_code == 201:
-                    print('Data uploaded successfully!')
-                else:
-                    print("Failed to upload data")
+                if 40 <= heart_rate <= 160:
+                    request_body = {"patient_id": patient_id, "heart_rate": int(float(heart_rate)), "body_temperature": int(float(body_temperature))}
+                    response = requests.post(api_url, json=request_body)
+                    if response.status_code == 201:
+                        print(response.json())
+                    else:
+                        print(response.json())
+                        print("Failed to upload data")
 except KeyboardInterrupt:
     print("Serial communication interrupted.")
 except requests.exceptions.RequestException as e:
